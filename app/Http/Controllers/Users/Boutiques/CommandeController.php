f0e9e761c8abb5db5058707cb17fcb580372bf4d
kepsonDiaz <kherabadiaby328@gmail.com>
2023-04-12 16:16:09 +0000

    commandes controller

---
+++ app/Http/Controllers/Users/Boutiques/CommandeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Users\Boutiques;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CommandeController extends Controller
+{
+    /**
+     * fonction qui permet voir les commandes d'un  utilisateur connecter
+     */
+    public function voirCommandes()
+    {
+        
+    }
+}
